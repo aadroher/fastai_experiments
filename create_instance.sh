547ef51685f2f4265e34b07d1bd21ae44550440e
Armand Adroher <aadroher@gmail.com>
2020-05-09 18:39:22 +0200

    Initial commit

---
+++ create_instance.sh
@@ -0,0 +1,10 @@
+gcloud compute instances create $INSTANCE_NAME \
+        --zone=$ZONE \
+        --image-family=$IMAGE_FAMILY \
+        --image-project=deeplearning-platform-release \
+        --maintenance-policy=TERMINATE \
+        --accelerator="type=nvidia-tesla-p100,count=1" \
+        --machine-type=$INSTANCE_TYPE \
+        --boot-disk-size=200GB \
+        --metadata="install-nvidia-driver=True" \
+        --preemptible
